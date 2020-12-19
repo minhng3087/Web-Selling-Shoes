74043ef90cdabb269951187cbce019f6107e1c23
minh <nguyengiaminh2k@gmail.com>
2020-12-19 23:44:44 +0700

    update

--- Frontend/assets/js/script.js
+++ Frontend/assets/js/script.js
@@ -1,14 +1,3 @@
-//assets/js/script.js
-// Do trang đã tích hợp jQuery -> dùng cú pháp của jQuery
-function number_format( number, decimals, dec_point, thousands_sep ) {
-
-    var n = number, c = isNaN(decimals = Math.abs(decimals)) ? 2 : decimals;
-    var d = dec_point == undefined ? "," : dec_point;
-    var t = thousands_sep == undefined ? "," : thousands_sep, s = n < 0 ? "-" : "";
-    var i = parseInt(n = Math.abs(+n || 0).toFixed(c)) + "", j = (j = i.length) > 3 ? j % 3 : 0;
-    
-    return s + (j ? i.substr(0, j) + t : "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + t) + (c ? d + Math.abs(n - i).toFixed(c).slice(2) : "");
-}
 $(document).ready(function () {
     // Cart hover (header)
     $('.add-to-cart').click(function() {
@@ -22,14 +11,6 @@ $(document).ready(function () {
             },
             success: function (data) {
                 console.log(data);
-                $('.ajax-message')
-                    .html('Thêm sản phẩm vào giỏ thành công');
-                $('.ajax-message')
-                    .addClass('ajax-message-active');
-                setTimeout(function() {
-                    $('.ajax-message')
-                        .removeClass('ajax-message-active');
-                }, 3000);
                 renderCart(data);
                 alertify.success('Thêm sản phẩm thành công');
             }
@@ -48,17 +29,9 @@ $(document).ready(function () {
             success: function (data) {
                 data = JSON.parse(data);
                 console.log(data);
-                $('.ajax-message')
-                    .html('Xóa sản phẩm thành công');
-                $('.ajax-message')
-                    .addClass('ajax-message-active');
-                setTimeout(function() {
-                    $('.ajax-message')
-                        .removeClass('ajax-message-active');
-                }, 3000);
-                alertify.success('Xóa sản phẩm thành công');
                 renderCart(data.cartHeader);
                 renderTableCart(data.cartTable);
+                alertify.success('Xóa sản phẩm thành công');
             }
         });
     });
@@ -76,19 +49,9 @@ $(document).ready(function () {
             },
             success: function (data) {
                 data = JSON.parse(data);
-                console.log(data);
-                $('.ajax-message')
-                    .html('Xóa sản phẩm thành công');
-                $('.ajax-message')
-                    .addClass('ajax-message-active');
-                setTimeout(function() {
-                    $('.ajax-message')
-                        .removeClass('ajax-message-active');
-                }, 3000);
-                alertify.success('Xóa sản phẩm thành công');
                 renderCart(data.cartHeader);
                 renderTableCart(data.cartTable);
-
+                alertify.success('Xóa sản phẩm thành công');
             }
         });
     });
